398d02a9fcaa90f22accaa93925842a4a32bb569
Cyrus <cyrusyong1@gmail.com>
2021-10-28 23:48:59 -0400

    kinematics finished

--- website/physics.py
+++ website/physics.py
@@ -1,3 +1,5 @@
+import math
+
 def no_x(vi, vf, a, t):
 
     if vf == "":
@@ -12,3 +14,45 @@ def no_x(vi, vf, a, t):
     if t == "":
         t = (float(vf) - float(vi)) / float(a)
         return "t = " + str(t)
+
+def no_a(x, vi, vf, t):
+    if x == "":
+        x = 0.5 * (float(vf) + float(vi)) * float(t)
+        return "x = " + str(x)
+    if vf == "":
+        vt = 2 * (float(x) / float(t)) - float(vi)
+        return "vf = " + str(vt)
+    if vi == "":
+        v0 = 2 * (float(x) / float(t)) - float(vf)
+        return "vi = " + str(v0)
+    if t == "":
+        t = (2 * float(x)) / (float(vf) + float(vi))
+        return "t = " + str(t)
+
+def no_t(x, vi, vf, a):
+    if a == "":
+        a = ((pow(float(vf), 2) - pow(float(vi), 2)) / (2 * float(x)))
+        return "a = " + str(a)
+    if x == "":
+        x = ((pow(float(vf), 2) - pow(float(vi), 2)) / (2 * float(a)))
+        return "x = " + str(x)
+    if vf == "":
+        vt = math.sqrt((2 * float(a) * float(x)) + pow(float(vi), 2))
+        return "vf = " + str(vt)
+    if vi == "":
+        v0 = math.sqrt((-2 * float(a) * float(x)) + pow(float(vf), 2))
+        return "vi = " + str(v0)
+
+def no_vf(x, vi, a, t):
+    if x == "":
+        x = (float(vi) * float(t)) + (0.5 * float(a) * pow(float(t), 2))
+        return "x = " + str(x)
+    if vi == "":
+        vo = ((2 * float(x)) - (float(a) * pow(float(t), 2))) / (2 * float(t))
+        return "vi = " + str(vo)
+    if t == "":
+        t = (-float(vi) + math.sqrt(2 * float(a) * float(x) + pow(float(vi), 2)))
+        return "t = " + str(t)
+    if a == "":
+        a = ((2 * float(x)) - (2 * float(vi) * float(t)))
+        return "a = " + str(a)
--- website/views.py
+++ website/views.py
@@ -1,37 +1,38 @@
 from flask import Blueprint, request, render_template
 from flask.templating import render_template
 from . import physics
+from website import physics
+
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET', 'POST'])
-def home():
-    answer = ''
-    vi = request.form.get("Vi")
-    vf = request.form.get("Vf")
+@views.route('/no_x', methods=['GET', 'POST'])
+def kinematics():
+
+    answer = 0
+    x = request.form.get("x")
+    vi = request.form.get("vi")
+    vf = request.form.get("vf")
     a = request.form.get("a")
     t = request.form.get("t")
 
-    if vi != '':
-        vi = float(vi)
-    else:
+    print(vi)
+    print(vf)
+    print(a)
+    print(t)
+
+    if x == "":
+        print("x")
         answer = physics.no_x(vi, vf, a, t)
+    if a == "":
+        print("a")
+        answer = physics.no_a(x, vi, vf, t)
+    if t == "":
+        print("t")
+        answer = physics.no_t(x, vi, vf, a)
+    if vf == "":
+        print("vf")
+        answer = physics.no_vf(x, vi, a, t)
 
-    if vf != '':
-        vf = float(vf)
-    else:
-        answer = physics.no_x(vi, vf, a, t)
+    return render_template("Kinematics.html", ans=answer)
 
-    if a != '':
-        a = float(a)
-    else:
-        answer = physics.no_x(vi, vf, a, t)
-
-    if t != '':
-        t = float(t)
-    else:
-        answer = physics.no_x(vi, vf, a, t)
-
-   
-
-    return render_template("no_x.html", ans=answer)
